06081f78d99bf666b9bc6e75f2de636b536a7c4f
rohan-005 <rohandhanerwal@gmail.com>
2025-11-04 10:48:29 +0530

    Navbar

--- src/components/bg/Custombg.jsx
+++ src/components/bg/Custombg.jsx
@@ -1,14 +1,32 @@
 import React from "react";
+import PillNav from "../Navbar";
 
 const Custombg = () => {
   return (
-    <div className="m-0 p-0 relative h-screen w-full bg-amber-300 flex flex-col items-center justify-center">
-      <div className="first w-[97%] h-[56%] bg-red-400 absolute top-7">
-        
+    <div className="m-0 p-0 relative h-screen w-full bg-amber-300 flex items-center justify-center">
+      <div className="pl-7 nav flex items-center justify-center bg-green-200 h-15 w-[39%] absolute top-7 right-7.5">
+        <PillNav
+          // logo={logo}
+          logoAlt="Company Logo"
+          items={[
+            { label: "Home", href: "/" },
+            { label: "About", href: "/about" },
+            { label: "Services", href: "/services" },
+            { label: "Contact", href: "/contact" },
+            { label: "Contact", href: "/contact" },
+          ]}
+          activeHref="/"
+          className="custom-nav"
+          ease="power2.easeOut"
+          baseColor="#000000"
+          pillColor="#ffffff"
+          hoveredPillTextColor="#ffffff"
+          pillTextColor="#000000"
+        />
       </div>
-      <div className="second w-[97%] h-[40%] bg-blue-400 absolute top-[56%]">
 
-      </div>
+      <div className="first w-[97%] h-[60%] bg-red-400 absolute top-7"></div>
+      <div className="second w-[97%] h-[40%] bg-blue-400 absolute top-[58%]"></div>
     </div>
   );
 };
